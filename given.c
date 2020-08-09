47c977640780d24b139c7baff3971276ecf0f200
NIRANJAN KUMAR M <37568654+stepin104545@users.noreply.github.com>
2020-08-09 23:20:25 +0530

    Update given.c

--- given.c
+++ given.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-char st[100];
-printf("write the string with '_' token\n");
-scanf("%s",st);
+char st[100]="my_name_is_edcast_future_skills";
+// or if you want to scan the different string means use the below printf and scanf.
+/*printf("write the string with '_' token\n");
+scanf("%s",st);*/
 char *a;
 a=strtok(st,"_");
 while(a!=NULL){
